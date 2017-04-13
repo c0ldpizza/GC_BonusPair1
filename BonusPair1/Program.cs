f98fa2cf72f1e14187ecd1f35f28bef0f46cbeb2
Charles Lambrecht <lambrechtca@gmail.com>
2017-04-13 14:25:35 -0400

    A through D

---
+++ BonusPair1/Program.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BonusPair1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //PrintA(6);
+            //PrintB(6);
+            //PrintC(6);
+            PrintD(6);
+        }
+
+        public static void PrintA(int input)
+        {
+            for (int i = input; i >= 1; i--)
+            {
+                for (int j = i; j <= input; j++)
+                {
+                    Console.Write("#");
+                }
+                Console.WriteLine();
+            }
+        }
+
+        public static void PrintB(int input)
+        {
+            for (int i = 1; i <= input; i++)
+            {
+                for (int j = i; j <= input; j++)
+                {
+                    Console.Write("#");
+                }
+                Console.WriteLine();
+            }
+        }
+        public static void PrintC(int input)
+        {
+            for (int i = input + 1; i >= 1; i--)
+            {
+                for (int j = 0; j <= input - i; j++)
+                {
+                    Console.Write(" ");
+                }
+
+                for (int k = 1; k <= i; k++)
+                {
+                    Console.Write("#");
+                }
+                Console.WriteLine();
+            }
+        }
+
+        public static void PrintD(int input)
+        {
+            for (int i = 1; i <= input; i++)
+            {
+                for (int j = input - i; j >= 1; j--)
+                {
+                    Console.Write(" ");
+                }
+
+                for (int k = 1; k <= i; k++)
+                {
+                    Console.Write("#");
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
